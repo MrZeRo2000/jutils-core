01f67d2ed5aa42968f8a79a0bea91c1b00182556
MrZeRo2000 <MrZeRo2000@mail.ru>
2021-01-19 17:40:51 +0200

    upgrade to gradle 6.5 and junit 5

---
+++ src/test/java/com/romanpulov/jutilscore/SampleTest.java
@@ -0,0 +1,12 @@
+package com.romanpulov.jutilscore;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    void testTrue(){
+        Assertions.assertTrue(true);
+    }
+}
