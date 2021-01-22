2f5b4e8868e181475dadfe09034d1a28f55d22ed
MrZeRo2000 <MrZeRo2000@mail.ru>
2021-01-22 14:06:24 +0200

    iss4 Implement tests for BackupUtils

--- src/main/java/com/romanpulov/jutilscore/storage/BackupUtils.java
+++ src/main/java/com/romanpulov/jutilscore/storage/BackupUtils.java
@@ -12,56 +12,36 @@ import java.io.FileFilter;
  */
 
 public class BackupUtils {
-    private final String mDataFileName;
-    private final String mBackupFolderName;
-    private final String mBackupFileName;
 
     /**
-     * Returns backup folder name from given folder name with ending slash
+     * Returns folder name from given folder name with ending slash
      * @param folderName input folder name
-     * @return backup folder name
+     * @return folder name with slash
      */
-    public static String getBackupFolderName(String folderName) {
+    public static String normalizeFolderName(String folderName) {
         if (folderName.endsWith(File.separator))
             return folderName;
         else
             return folderName + File.separator;
-    }
-    /**
-     * Returns backup folder name with ending slash
-     * @return backup folder name
-     */
-    private String getBackupFolderName() {
-        return getBackupFolderName(mBackupFolderName);
     }
 
     /**
      * Returns backup file name
      * @return file name
      */
-    private String getBackupFileName() {
-        return getBackupFolderName() + mBackupFileName;
-    }
-
-    private String getBackupZIPFileName() {
-        return ZipFileUtils.getZipFileName(getBackupFileName());
-    }
-
-    public BackupUtils(String dataFileName, String backupFolderName, String backupFileName) {
-        mDataFileName = dataFileName;
-        mBackupFolderName = backupFolderName;
-        mBackupFileName = backupFileName;
+    private static String getFullFileName(String folderName, String fileName) {
+        return normalizeFolderName(folderName) + fileName;
     }
 
     /**
      * Created local backup
      * @return archived file name if successful
      */
-    private String createLocalBackup() {
-        String backupFileName = getBackupFileName();
+    private static String createLocalBackup(String dataFileName, String backupFolderName, String backupFileName) {
+        String backupFullFileName = getFullFileName(backupFolderName, backupFileName);
 
         //init backup folder
-        File backupFolder = new File(getBackupFolderName());
+        File backupFolder = new File(normalizeFolderName(backupFolderName));
         if (!backupFolder.exists()) {
             if (!backupFolder.mkdir()) {
                 return null;
@@ -69,56 +49,56 @@ public class BackupUtils {
         }
 
         //write file
-        if (!backupFileName.equals(mDataFileName)) {
-            if (!FileUtils.copy(mDataFileName, backupFileName))
+        if (!backupFullFileName.equals(dataFileName)) {
+            if (!FileUtils.copy(dataFileName, backupFullFileName))
                 return null;
         }
 
         //archive file
-        return ZipFileUtils.zipFile(getBackupFolderName(), mBackupFileName);
+        return ZipFileUtils.zipFile(normalizeFolderName(backupFolderName), backupFileName);
     }
 
     /**
      * Restores backup from archive
      * @return restored file name if successful
      */
-    public String restoreBackup() {
-        String backupFileName = getBackupFileName();
+    public static String restoreBackup(String dataFileName, String backupFolderName, String backupFileName) {
+        String backupFullFileName = getFullFileName(backupFolderName, backupFileName);
 
         //check backup availability
-        File zipFile = new File(getBackupZIPFileName());
+        File zipFile = new File(ZipFileUtils.getZipFileName(backupFullFileName));
         if (!zipFile.exists())
             return null;
 
         //extract backup
-        if (!ZipFileUtils.unZipFile(getBackupFolderName(), ZipFileUtils.getZipFileName(mBackupFileName)))
+        if (!ZipFileUtils.unZipFile(normalizeFolderName(backupFolderName), ZipFileUtils.getZipFileName(backupFileName)))
             return null;
 
         //check restored file availability
-        File file = new File(backupFileName);
+        File file = new File(backupFullFileName);
         if (!file.exists())
             return null;
 
-        if (!backupFileName.equals(mDataFileName)) {
+        if (!backupFileName.equals(dataFileName)) {
             //replace source file
-            if (!FileUtils.copy(backupFileName, mDataFileName))
+            if (!FileUtils.copy(backupFullFileName, dataFileName))
                 return null;
 
             //delete and ignore any errors
             file.delete();
         }
 
-        return backupFileName;
+        return dataFileName;
     }
 
     /**
      * Created rolling backup
      * @return archived file if successful
      */
-    public String createRollingLocalBackup() {
+    public static String createRollingLocalBackup(String dataFileName, String backupFolderName, String backupFileName) {
         //get file names
-        String fileName = getBackupFileName();
-        String zipFileName = getBackupZIPFileName();
+        String fileName = getFullFileName(backupFolderName, backupFileName);
+        String zipFileName = ZipFileUtils.getZipFileName(fileName);
 
         //roll copies of data: first try rename, then copy
         if (!FileUtils.renameCopies(zipFileName))
@@ -126,7 +106,7 @@ public class BackupUtils {
                 return null;
 
         //create backup
-        String result = createLocalBackup();
+        String result = createLocalBackup(dataFileName, backupFolderName, backupFileName);
 
         //delete non zipped file, ignore any errors
         if (result != null)
@@ -135,18 +115,20 @@ public class BackupUtils {
         return result;
     }
 
+    public static class BackupFileFilter implements FileFilter {
+        @Override
+        public boolean accept(File pathname) {
+            String lowAbsolutePath = pathname.getAbsolutePath().toLowerCase();
+            return lowAbsolutePath.endsWith(ZipFileUtils.ZIP_EXT) || lowAbsolutePath.matches("\\S*" +ZipFileUtils.ZIP_EXT + "." + FileUtils.BAK_EXT + "" + "[0-9]{2}");
+        }
+    }
+
     /**
      * Returns backup files from backup folder
      * @return File list
      */
-    public File[] getBackupFiles() {
-        File folder = new File(getBackupFolderName());
-        return folder.listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File pathname) {
-                String lowAbsolutePath = pathname.getAbsolutePath().toLowerCase();
-                return lowAbsolutePath.endsWith(ZipFileUtils.ZIP_EXT) || lowAbsolutePath.matches("\\S*" +ZipFileUtils.ZIP_EXT + "." + FileUtils.BAK_EXT + "" + "[0-9]{2}");
-            }
-        });
+    public static File[] getBackupFiles(String backupFolderName) {
+        File folder = new File(normalizeFolderName(backupFolderName));
+        return folder.listFiles(new BackupFileFilter());
     }
 }
---
+++ src/test/java/com/romanpulov/jutilscore/BackupUtilsTest.java
@@ -0,0 +1,115 @@
+package com.romanpulov.jutilscore;
+
+import com.romanpulov.jutilscore.storage.BackupUtils;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+
+public class BackupUtilsTest {
+    private static final Path folderPath = Paths.get(System.getProperty("java.io.tmpdir") + "jutils-core-file-utils-backup-test/");
+
+    static void clearFolder(Path folder) throws Exception {
+        if (Files.exists(folder)) {
+            Files.walk(folder)
+                    .filter(path -> !Files.isDirectory(path))
+                    .forEach(path -> {
+                        System.out.println("Deleting file:" + path.toAbsolutePath().toString());
+                        try {
+                            Files.deleteIfExists(path);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+            Files.walk(folder)
+                    .filter(path -> Files.isDirectory(path) && !path.equals(folder))
+                    .forEach(path -> {
+                        System.out.println("Deleting directory:" + path.toAbsolutePath().toString());
+                        try {
+                            Files.deleteIfExists(path);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+            Files.delete(folder);
+        }
+    }
+
+    @BeforeAll
+    static void beforeAll() throws Exception {
+        System.out.println("Starting BackupUtilsTest: clearing folder");
+        clearFolder(folderPath);
+
+        Files.createDirectory(folderPath);
+        System.out.println("Folder " + folderPath.toAbsolutePath().toString() + " created");
+    }
+
+    @AfterAll
+    static void afterAll() throws Exception {
+        //clearFolder(folderPath);
+        //System.out.println("Finalizing BackupUtilsTest: folder cleared");
+    }
+
+    @Test
+    void mainTest() throws Exception {
+        final Path filePath  = Paths.get(folderPath.toString(), "data-file.bin");
+
+        // generate and write random bytes file
+
+        byte[] b1 = new byte[2056];
+        new Random().nextBytes(b1);
+
+        Assertions.assertFalse(Files.exists(filePath));
+
+        try(ByteArrayInputStream inputStream = new ByteArrayInputStream(b1)) {
+            Files.copy(inputStream, filePath);
+        }
+
+        Assertions.assertTrue(Files.exists(filePath));
+
+        String dataFileName = filePath.toAbsolutePath().toString();
+        String backupFolderName = filePath.getParent().toAbsolutePath().toString() + "/backup";
+        String backupFileName = "data-file-backup.bin";
+
+        Assertions.assertNotNull(BackupUtils.createRollingLocalBackup(dataFileName, backupFolderName, backupFileName));
+
+        // generate and write random bytes file 2
+
+        byte[] b2 = new byte[2012];
+        new Random().nextBytes(b2);
+
+        Files.deleteIfExists(filePath);
+
+        Assertions.assertFalse(Files.exists(filePath));
+
+        try(ByteArrayInputStream inputStream = new ByteArrayInputStream(b2)) {
+            Files.copy(inputStream, filePath);
+        }
+
+        Assertions.assertTrue(Files.exists(filePath));
+
+        Assertions.assertNotNull(BackupUtils.createRollingLocalBackup(dataFileName, backupFolderName, backupFileName));
+
+        // restoring from backup
+
+        Files.deleteIfExists(filePath);
+        Assertions.assertFalse(Files.exists(filePath));
+
+        Assertions.assertNotNull(BackupUtils.restoreBackup(dataFileName, backupFolderName, backupFileName));
+
+        Assertions.assertTrue(Files.exists(filePath));
+
+        // check restored file content, should be as b2
+
+        Assertions.assertArrayEquals(b2, Files.readAllBytes(filePath));
+
+    }
+
+}
