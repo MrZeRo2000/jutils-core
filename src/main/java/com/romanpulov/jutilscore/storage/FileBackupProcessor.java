56af5510ab5ef61e84a57161985bc128a47e9274
MrZeRo2000 <MrZeRo2000@mail.ru>
2021-01-25 14:28:10 +0200

    FileBackupProcessor variables rename, version 0.3.4

--- src/main/java/com/romanpulov/jutilscore/storage/FileBackupProcessor.java
+++ src/main/java/com/romanpulov/jutilscore/storage/FileBackupProcessor.java
@@ -3,28 +3,28 @@ package com.romanpulov.jutilscore.storage;
 import java.util.List;
 
 public class FileBackupProcessor implements BackupProcessor {
-    private final String mDataFileName;
-    private final String mBackupFolderName;
-    private final String mBackupFileName;
+    private final String dataFileName;
+    private final String backupFolderName;
+    private final String backupFileName;
 
-    public FileBackupProcessor(String mDataFileName, String mBackupFolderName, String mBackupFileName) {
-        this.mDataFileName = mDataFileName;
-        this.mBackupFolderName = mBackupFolderName;
-        this.mBackupFileName = mBackupFileName;
+    public FileBackupProcessor(String dataFileName, String backupFolderName, String backupFileName) {
+        this.dataFileName = dataFileName;
+        this.backupFolderName = backupFolderName;
+        this.backupFileName = backupFileName;
     }
 
     @Override
     public String createRollingBackup() {
-        return BackupUtils.createRollingLocalBackup(mDataFileName, mBackupFolderName, mBackupFileName);
+        return BackupUtils.createRollingLocalBackup(dataFileName, backupFolderName, backupFileName);
     }
 
     @Override
     public String restoreBackup() {
-        return BackupUtils.restoreBackup(mDataFileName, mBackupFolderName, mBackupFileName);
+        return BackupUtils.restoreBackup(dataFileName, backupFolderName, backupFileName);
     }
 
     @Override
     public List<String> getBackupFileNames() {
-        return BackupUtils.getBackupFileNames(mBackupFolderName);
+        return BackupUtils.getBackupFileNames(backupFolderName);
     }
 }
